d4e592421dbbc270f9ba4ef531fcc2a4ec44d368
ShawnBorn-Newton <shawn.newton3@gmail.com>
2018-07-15 17:11:11 -0400

    Modules with json

--- C6_LetsRunSomeJavascript/Starter/app.js
+++ C6_LetsRunSomeJavascript/Starter/app.js
@@ -1,13 +1,4 @@
-function Person(firstname, lastname) {
+var greet = require('./greet');
 
-  this.firstname = firstname;
-  this.lastname = lastname;
-}
-
-Person.prototype.greet = function(){
-  console.log('Hello, ' + this.firstname + ' '
-+ this.lastname);
-};
-
-var john = new Person('John', 'Doe');
-john.greet();
+greet.english();
+greet.spanish();
